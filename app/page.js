2efee56abe755282c004403d17399e319db10612
Amanj01 <ensw00223@student.su.edu.krd>
2025-02-28 22:38:06 +0300

    fixing logo slider

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ const page = () => {
       <div className='pt-16 md:pt-28 lg:pt-36'>
        <Carousel comments={comments} title={"user testimonials"} brandAppearance={true} />
       </div>
-      <div className='py-16 md:py-24'>
+      <div className='pb-14 pt-20 md:pb-24 md:pt-36'>
          <LogoSlider />
       </div>
     </div>
